8cc8631f879749bd15f763904e579f6a2e45fcf6
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2024-04-29 20:59:12 +0530

    Added separate controller and bindings for the signup page.

---
+++ src/main/java/io/github/bosev/flight_booking_gradle/SignupController.java
@@ -0,0 +1,22 @@
+package io.github.bosev.flight_booking_gradle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignupController {
+	@FXML
+	private TextField usernameField;
+
+	@FXML
+	private PasswordField passwordField;
+
+	@FXML
+	private PasswordField passwordConfirmationField;
+
+	@FXML
+	private TextField emailField;
+
+	@FXML
+	private TextField phoneField;
+}
