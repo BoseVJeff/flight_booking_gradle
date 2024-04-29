5790149b64e52a7a7a63df2f1998370358be8770
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2024-04-29 21:22:32 +0530

    Added bindings for the signup and login page buttons

--- src/main/java/io/github/bosev/flight_booking_gradle/LoginController.java
+++ src/main/java/io/github/bosev/flight_booking_gradle/LoginController.java
@@ -1,6 +1,7 @@
 package io.github.bosev.flight_booking_gradle;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class LoginController {
@@ -9,4 +10,10 @@ public class LoginController {
 
 	@FXML
 	private TextField password;
+
+	@FXML
+	private Button loginButton;
+
+	@FXML
+	private Button signupButton;
 }
