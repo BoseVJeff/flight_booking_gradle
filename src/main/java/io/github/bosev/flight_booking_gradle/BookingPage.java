984d39c837f8050f73db75915d503a59bc660d2a
Arpan Parekh <arpanparekh06@gmail.com>
2024-05-02 10:23:13 +0530

    modified Flight_booking.fxml

--- src/main/java/io/github/bosev/flight_booking_gradle/BookingPage.java
+++ src/main/java/io/github/bosev/flight_booking_gradle/BookingPage.java
@@ -1,10 +1,10 @@
 package io.github.bosev.flight_booking_gradle;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+
 
 public class BookingPage {
 
@@ -12,15 +12,14 @@ public class BookingPage {
     VBox vboxForButtons = new VBox();
 
     public void initialize() {
-            for (int i=1;i>10;i++) {
-                Button btnNumber = new Button();
-                btnNumber.setText(String.valueOf(i));
-                btnNumber.setOnAction(actionEvent -> {
-                    System.out.println(btnNumber.getText());
-                });
+        for (int i=1;i>10;i++) {
+            Button btnNumber = new Button();
+            btnNumber.setText(String.valueOf(i));
+            btnNumber.setOnAction(actionEvent -> {
+                System.out.println(btnNumber.getText());
+            });
 
-                vboxForButtons.getChildren().add(btnNumber);
-            }
+            vboxForButtons.getChildren().add(btnNumber);
+        }
     }
-
 }
